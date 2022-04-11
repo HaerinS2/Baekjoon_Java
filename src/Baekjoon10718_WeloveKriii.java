b613dc89e0f995fb77720f7e5b7ee3a7b1de98a2
Haerin <soundzrinjhr@gmail.com>
2022-04-11 14:14:56 +0900

    Baekjoon

---
+++ src/Baekjoon10718_WeloveKriii.java
@@ -0,0 +1,7 @@
+public class Baekjoon10718_WeloveKriii {
+    public static void main(String[] args) {
+        for(int i=1; i<=2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
