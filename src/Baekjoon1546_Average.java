dd7e7c12f7f201bc23924be9c5b99bac17d4e859
Haerin <soundzrinjhr@gmail.com>
2022-04-12 21:04:02 +0900

    Baekjoon

---
+++ src/Baekjoon1546_Average.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon1546_Average {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer s = null;
+        int n=Integer.parseInt(r.readLine());
+        float[] a=new float[n];
+        s = new StringTokenizer(r.readLine());
+        float res=0;
+        float sum=0;
+        float m=0;
+        for(int i=0; i<n; i++) {
+            a[i]=Integer.parseInt(s.nextToken());
+            for(int j=0; j<n; j++){
+                if(a[j]>m) {
+                    m = a[j];
+                }}}
+        for(int i=0; i<n; i++){
+            a[i]=(float)a[i]/m*100;
+            sum+=a[i];
+        }
+        res=sum/n;
+        System.out.println(res);
+
+
+    }
+}
