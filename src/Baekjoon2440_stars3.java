179c2026b7d869d7e4b241d2924beff1c6a1066a
haerin <soundzrinjhr@gmail.com>
2022-04-16 00:30:14 +0900

    Baekjoon

--- src/Baekjoon2440_stars3.java
+++ src/Baekjoon2440_stars3.java
@@ -1,2 +1,15 @@
+import java.io.*;
+
 public class Baekjoon2440_stars3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(r.readLine());
+        for (int i = 0; i < n; i++) {
+            for(int j=0; j<n-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
