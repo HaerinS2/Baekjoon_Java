39bae2490877767e4f272e80b9a91c57b27eb9b7
Haerin <soundzrinjhr@gmail.com>
2022-04-25 10:36:06 +0900

    Baekjoon

---
+++ src/Baekjoon1463_makeOne.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Baekjoon1463_makeOne {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        }
+        System.out.print(cnt);
+    }
+}
