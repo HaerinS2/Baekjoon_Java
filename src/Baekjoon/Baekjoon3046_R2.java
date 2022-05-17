ff3c6bc7efe70d8e4123efa06c77c332edfb3cba
Haerin <soundzrinjhr@gmail.com>
2022-05-17 19:19:00 +0900

    Baekjoon

---
+++ src/Baekjoon/Baekjoon3046_R2.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon3046_R2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int r2 = (2*s - r1);
+        System.out.println(r2);
+    }
+}
