9a97b7110033565f1bdb1fa5d4a6af82b36fea29
Haerin <soundzrinjhr@gmail.com>
2022-10-12 11:26:46 +0900

    Baekjoon 꼬마 정민

---
+++ src/Baekjoon/Baekjoon11382.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Baekjoon11382 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        BigInteger c = new BigInteger(st.nextToken());
+
+        bw.write(String.valueOf((a.add(b).add(c))));
+        bw.flush();
+        bw.close();
+    }
+}
