08f665e958bcfdb2cb24eac635b10b8d96deb9f6
Haerin <soundzrinjhr@gmail.com>
2022-10-12 12:55:05 +0900

    Baekjoon 홍익대학교

---
+++ src/Baekjoon/Baekjoon16394.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baekjoon16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int var=n-1946;
+        System.out.println(var);
+    }
+}
