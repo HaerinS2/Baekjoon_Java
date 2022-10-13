871aaf2ed529453e80b6cde6c2800bdfe9853917
Haerin <soundzrinjhr@gmail.com>
2022-10-13 10:31:03 +0900

    Baekjoon 엔드게임 스포일러

---
+++ src/Baekjoon/Baekjoon17295.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon17295 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String story = br.readLine();
+
+        bw.write("Avengers: Endgame");
+        bw.flush();
+        bw.close();
+    }
+}
