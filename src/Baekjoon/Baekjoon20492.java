a10210ce4248ff785aa0d9074c2c53a43738e2b0
Haerin <soundzrinjhr@gmail.com>
2022-10-17 17:27:16 +0900

    Baekjoon 세금

---
+++ src/Baekjoon/Baekjoon20492.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon20492 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf((int)(n*0.78)) + " " + String.valueOf((int)(n-(n*0.2*0.22))));
+        bw.flush();
+        bw.close();
+    }
+}
